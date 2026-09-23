f1d7116108effa79b1e45ae2ef99a8125e06e35e
gucceed <gucceed@gmail.com>
2026-09-22 22:29:06 -0400

    Add sirene_tasks to ingestion tasks list

--- celeryapp.py
+++ celeryapp.py
@@ -19,6 +19,7 @@ app.autodiscover_tasks([
     "ingestion.tasks.cvr_tasks",
     "ingestion.tasks.brreg_tasks",
     "ingestion.tasks.prh_tasks",
+    "ingestion.tasks.sirene_tasks",
     "kreditvakt.signal_cross",
     "kreditvakt.contagion",
 ], related_name=None, force=True)
